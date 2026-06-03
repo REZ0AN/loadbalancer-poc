63772d57675ab658b818975966f766cc2901a8ee
REZ0AN <ahmedabir.rez@gmail.com>
2026-06-04 03:39:46 +0600

    (feat/topology): add new client to the nodes for testing multi client suppot in xdp-lb-flow-table

--- network-namespaces-mental-model/topology.sh
+++ network-namespaces-mental-model/topology.sh
@@ -7,6 +7,7 @@ SUBNET="10.0.0.0/24"
 # Format: "<netns_name> <ip_in_subnet>"
 NODES=(
     "client 10.0.0.10"
+    "clientX 10.0.0.20"
     "lb     10.0.0.5"
     "b1     10.0.0.11"
     "b2     10.0.0.12"
